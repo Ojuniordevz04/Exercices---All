fde569e19969ff59d522e29d8150c16d3b7e302d
Williams Junior <112410570+Ojuniordevz04@users.noreply.github.com>
2022-08-29 12:21:18 -0300

    Add files via upload

---
+++ matematica.py
@@ -0,0 +1,36 @@
+#CALCULAR PERCENTUAL
+def calcula_percentual(n1_total, n2_perc):
+        percentual = (n2_perc * 100) / n1_total
+        return percentual
+#CALCULAR DIVISÃO
+def divide_dois(n1, n2):
+    divisao = (n1 / n2)
+    return divisao
+#CALCULAR FATORIAL
+def fatorial():
+    pass
+#CALCULAR MEDIA
+def media(n1,n2):
+    media = n1 + n2
+    return media
+#CALCULAR MULTIPLICAÇÃO
+def multiplica_dois(n1,n2):
+    
+#PAR OU IMPA
+    def par_ou_impar():
+     pass
+#CALCULAR POTENCIAÇÃO
+def potencia():
+    pass
+
+#CALCULAR QUANTOS PORCENTO
+def quantos_porcento():
+    pass
+
+#SOMAR DOIS NUMEROS
+def soma_dois():
+    pass
+
+#SUBTRAIR DOIS NUMEROS
+def subtrai_dois():
+    pass
---
+++ principal.py
@@ -0,0 +1,32 @@
+from matematica import *
+#PERCENTUAL
+print("FAZENDO O PERCENTUAL DE UM NÚMERO: ")
+n1 = float(input("Informe o número total: "))
+n2 = float(input("Informe o percentual: "))
+calcula_percentual(n1,n2)
+print(calcula_percentual(n1,n2))
+print("-"*20)
+
+#DIVISAO
+print("DIVIDINDO DOIS NÚMEROS: ")
+n1 = float(input("Informe o número total: "))
+n2 = float(input("Informe o número total: "))
+divide_dois(n1,n2)
+print(divide_dois(n1,n2))
+print("-"*20)#'''
+#FATORIAL
+
+#MEDIA
+print("CALCULANDO A MEDIA")
+n1 = float(input("Informe a nota 1: "))
+n2 = float(input("Informe a nota 2: "))
+med = int(input("Informe a média: "))
+media (n1,n2 / med)
+print(f"{media(n1,n2 / med)}")
+
+#MULTIPLICAÇÃO
+print("MULTPLICANDO DOIS NÚMEROS")
+n1 = int(input("Informe o número: "))
+n2 = int(input("Informe o segundo número: "))
+Multiplica_dois(n1,n2)
+print(Multiplica_dois(n1,n2))
